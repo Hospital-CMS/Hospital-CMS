be3004cbbf60ba930e781b636c16d8a2cb7e2eec
gazzen <gazensuwal@gmail.com>
2023-04-18 23:32:46 -0400

    update

--- Hospital-CMS/Controllers/DischargeController.cs
+++ Hospital-CMS/Controllers/DischargeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Diagnostics;
+using Hospital_CMS.Models;
+using System.Web.Script.Serialization;
 
 namespace Hospital_CMS.Controllers
 {
